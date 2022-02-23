84221eac6a517adf460db223b60a732e256b733e
wuye <ye.wu@ppiaas.cn>
2022-02-23 11:31:09 +0800

    add trader service on the same Server

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 
+use trading::trade_server::{Trade, TradeServer};
+use trading::{TradeReply, TradeRequest};
 use user_center::user_server::{User, UserServer};
 use user_center::{LoginReply, LoginRequest};
 
@@ -12,16 +14,32 @@ pub struct MyUser {}
 
 #[tonic::async_trait]
 impl User for MyUser {
-    async fn login(
-        &self,
-        request: Request<LoginRequest>,
-    ) -> Result<Response<LoginReply>, Status> {
+    async fn login(&self, request: Request<LoginRequest>) -> Result<Response<LoginReply>, Status> {
         println!("Got a request: {:?}", request);
         let req = request.into_inner();
         let reply = user_center::LoginReply {
             message: format!("Hello {}!, your password is {}", req.name, req.password).into(),
         };
 
+        Ok(Response::new(reply))
+    }
+}
+
+pub mod trading {
+    tonic::include_proto!("trading");
+}
+
+#[derive(Debug, Default)]
+pub struct Trader {}
+
+#[tonic::async_trait]
+impl Trade for Trader {
+    async fn trade(&self, request: Request<TradeRequest>) -> Result<Response<TradeReply>, Status> {
+        println!("request: {:?}", request);
+        let req = request.into_inner();
+        let reply = trading::TradeReply{ message: 
+            format!("trading successful!, from: {}, to: {}, amount: {}", req.from, req.to, req.amount,).into(),
+        };
         Ok(Response::new(reply))
     }
 }
@@ -33,6 +51,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Server::builder()
         .add_service(UserServer::new(user))
+        .add_service(TradeServer::new(Trader::default()))
         .serve(addr)
         .await?;
 
